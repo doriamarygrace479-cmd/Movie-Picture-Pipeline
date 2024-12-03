e33637e6bd048c8eed8bfb20120d9fcc81f3e25b
AnhTung <anhtung6950@gmail.com>
2024-12-04 01:59:27 +0700

    commit 12

--- starter/frontend/src/components/__tests__/App.test.js
+++ starter/frontend/src/components/__tests__/App.test.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 import App from '../../App';
 
-const movieHeading = process.env.FAIL_TEST ? 'WRONG_HEADING' : 'Movie Lists test5';
+const movieHeading = process.env.FAIL_TEST ? 'WRONG_HEADING' : 'Movie Lists test6';
 
 test('renders Movie List heading', () => {
   render(<App />);
