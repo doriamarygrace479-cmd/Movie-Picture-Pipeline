048e36f10c4a5edc2a9d44a6b71c2520c91a851c
xwll8 <layanalghamdi.mohammed@gmail.com>
2026-01-05 20:56:13 +0300

    first commit

--- setup/terraform/versions.tf
+++ setup/terraform/versions.tf
@@ -1,9 +1,10 @@
 provider "aws" {
   region = "us-east-1"
+  profile = "udacity-terr"
 }
 
 terraform {
-  required_version = "1.3.9"
+  required_version = ">= 1.3.9"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
